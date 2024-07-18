2f57bd799bdb9232eadde7629ac53ed68ef05e33
JongHyunLee84 <whdgus3697@naver.com>
2024-07-19 00:16:11 +0900

    design: CategoryText

--- Sources/CommonUI/Components/CategoryText.swift
+++ Sources/CommonUI/Components/CategoryText.swift
@@ -1,18 +1,33 @@
-//
-//  SwiftUIView.swift
-//  
-//
-//  Created by 이종현 on 7/18/24.
-//
-
+import Models
 import SwiftUI
 
-struct SwiftUIView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+public struct CategoryText: View {
+    public let noteCategory: String
+    public let noteColor: Color
+    
+    public init(
+        noteCategory: String,
+        noteColor: Color
+    ) {
+        self.noteCategory = noteCategory
+        self.noteColor = noteColor
+    }
+    
+    public var body: some View {
+        Text(noteCategory)
+            .textStyler(color: .white,
+                        font: .caption,
+                        weight: .black)
+            .border(noteColor,
+                    backgroundColor: noteColor,
+                    radius: 30,
+                    verticalPadding: 6,
+                    horizontalPadding: 20)
+
     }
 }
 
 #Preview {
-    SwiftUIView()
+    CategoryText(noteCategory: "식품",
+                 noteColor: .mainDarkBlue)
 }
--- Sources/CommonUI/Components/ReviewStars.swift
+++ Sources/CommonUI/Components/ReviewStars.swift
@@ -1,18 +1,43 @@
-//
-//  SwiftUIView.swift
-//  
-//
-//  Created by 이종현 on 7/18/24.
-//
-
+import Models
 import SwiftUI
 
-struct SwiftUIView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+public struct ReviewStars: View {
+    public let reviewScore: Double
+    public let font: Font
+    
+    public init(
+        reviewScore: Double,
+        font: Font
+    ) {
+        self.reviewScore = reviewScore
+        self.font = font
+    }
+    public var body: some View {
+        HStack(spacing: 0) {
+            ForEach(1...5, id: \.self) { idx in
+                let intValue = Int(reviewScore)
+                if idx <= intValue {
+                    Image(systemName: "star.fill")
+                        .textStyler(color: .yellow,
+                                    font: font)
+                } else {
+                    Image(systemName: "star")
+                        .textStyler(color: .yellow,
+                                    font: font)
+                }
+            }
+            Spacer()
+        }
     }
 }
 
 #Preview {
-    SwiftUIView()
+    VStack {
+        ReviewStars(reviewScore: 5, font: .footnote)
+        ReviewStars(reviewScore: 4, font: .footnote)
+        ReviewStars(reviewScore: 3, font: .footnote)
+        ReviewStars(reviewScore: 2, font: .footnote)
+        ReviewStars(reviewScore: 1, font: .footnote)
+    }
+    .padding(.leading)
 }
