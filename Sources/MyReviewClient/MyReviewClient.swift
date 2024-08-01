a13b7f6432917e762b9803ed2014606199253ef4
JongHyunLee84 <whdgus3697@naver.com>
2024-08-01 11:23:41 +0900

    feat: MyReviewClient + Live Dependency

--- Sources/MyReviewClient/MyReviewClient.swift
+++ Sources/MyReviewClient/MyReviewClient.swift
@@ -1,8 +1,28 @@
-//
-//  File.swift
-//  
-//
-//  Created by 이종현 on 8/1/24.
-//
-
+import Dependencies
+import DependenciesMacros
 import Foundation
+import Models
+
+@DependencyClient
+public struct MyReviewClient {
+    public var getReviewList: (_ userID: String) async throws -> ReviewList
+    public var postReview: (_ userID: String, _ review: Review) async throws -> Void
+    public var deleteReview: (_ userID: String, _ reviewID: String) async throws -> Void
+}
+
+extension DependencyValues {
+    public var myReviewClient: MyReviewClient {
+        get { self[MyReviewClient.self] }
+        set { self[MyReviewClient.self] = newValue }
+    }
+}
+
+extension MyReviewClient: TestDependencyKey {
+    public static var testValue = Self(
+        getReviewList: { _ in .mock },
+        postReview: { _, _ in },
+        deleteReview: { _, _ in }
+    )
+    
+    public static var previewValue = testValue
+}
--- Sources/MyReviewClientLive/MyReviewClientLive.swift
+++ Sources/MyReviewClientLive/MyReviewClientLive.swift
@@ -1,8 +1,41 @@
-//
-//  File.swift
-//  
-//
-//  Created by 이종현 on 8/1/24.
-//
+import Dependencies
+import FirebaseFirestore
+import MyReviewClient
+import Models
 
-import Foundation
+extension MyReviewClient: DependencyKey {
+    public static var liveValue = Self(
+        getReviewList: { userID in
+            let snapshot = try await getReviewCollection(userID)
+                .getDocuments()
+            return try snapshot.documents.tryMap { try $0.data(as: Review.self) }
+        },
+        postReview: { userID, review in
+            try getReviewDocument(userID, review.id)
+                .setData(from: review)
+        },
+        deleteReview: { userID, reviewID in
+            getReviewDocument(userID, reviewID)
+                .delete()
+        }
+    )
+}
+
+fileprivate let database = Firestore.firestore()
+fileprivate let userCollection = database.collection("users")
+
+fileprivate func getReviewCollection(
+    _ userID: String
+) -> CollectionReference {
+    userCollection
+        .document(userID)
+        .collection("reviews")
+}
+
+fileprivate func getReviewDocument(
+    _ userID: String,
+    _ reviewID: String
+) -> DocumentReference {
+    getReviewCollection(userID)
+        .document(reviewID)
+}
