091a482eded392d4111a1c5b683b6e3d02544705
JongHyunLee84 <whdgus3697@naver.com>
2024-08-05 22:35:55 +0900

    feat: URLClient, Live

--- Sources/URLClient/URLClient.swift
+++ Sources/URLClient/URLClient.swift
@@ -1,8 +1,30 @@
-//
-//  File.swift
-//  
-//
-//  Created by 이종현 on 8/5/24.
-//
-
+import Dependencies
+import DependenciesMacros
 import Foundation
+
+@DependencyClient
+public struct URLClient {
+    public var getIntroduceURL: @Sendable () async -> URL?
+    public var getPrivacyPolicyURL: @Sendable () async -> URL?
+    public var getCSURL: () async -> URL?
+}
+
+extension DependencyValues {
+    public var urlClient: URLClient {
+        get { self[URLClient.self] }
+        set { self[URLClient.self] = newValue }
+    }
+}
+
+extension URLClient: TestDependencyKey {
+    public static var testValue = Self(
+            getIntroduceURL: { mockURL },
+            getPrivacyPolicyURL: { mockURL },
+            getCSURL: { mockURL }
+    )
+    
+    public static var previewValue = testValue
+}
+
+public let mockURL = URL(string: "https://www.google.com")!
+
--- Sources/URLClientLive/URLClientLive.swift
+++ Sources/URLClientLive/URLClientLive.swift
@@ -1,8 +1,38 @@
-//
-//  File.swift
-//  
-//
-//  Created by 이종현 on 8/5/24.
-//
-
+import Dependencies
 import Foundation
+import KakaoSDKTalk
+import URLClient
+
+extension URLClient: DependencyKey {
+    public static var liveValue: Self {
+        let secrets = Secrets.load()
+        return Self(
+            getIntroduceURL: { 
+                guard let introduceURLStr = secrets?.introduceURL else { return nil }
+                return URL(string: introduceURLStr)!
+            },
+            getPrivacyPolicyURL: { 
+                guard let privacyPolicyURLStr = secrets?.privacyPolicyURL else { return nil }
+                return URL(string: privacyPolicyURLStr)!
+            },
+            getCSURL: {
+                guard let kakaoPublicID = secrets?.kakaoChannelPublicId else { return nil }
+                return TalkApi.shared.makeUrlForChatChannel(channelPublicId: kakaoPublicID)
+            }
+        )
+    }
+}
+
+struct Secrets: Decodable {
+    let introduceURL: String
+    let privacyPolicyURL: String
+    let kakaoChannelPublicId:  String
+    
+    static func load() -> Self? {
+        guard let secretsFileURL = Bundle.module.url(forResource: "secrets", withExtension: "json"),
+              let data = try? Data(contentsOf: secretsFileURL) else {
+            return nil
+        }
+        return try? JSONDecoder().decode(Self.self, from: data)
+    }
+}
